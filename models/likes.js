da7031834be55441ffde54e3eb0a41bd396b5a91
Rishabh2728 <rishabh27kumar2006@gmail.com>
2026-02-13 00:20:42 +0530

    likes bug-fix

--- models/likes.js
+++ models/likes.js
@@ -1,17 +1,22 @@
 import mongoose from "mongoose";
 
 const likeSchema = new mongoose.Schema(
-    {
-        artifact: {
-            type: mongoose.Schema.Types.ObjectId,
-            rof: "Artifact",
-            required: true
-        },
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true
-        }
+  {
+    artifact: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Artifact",
+      required: true
     },
-    {timestamps: true}
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
 );
+
+// Prevent duplicate likes
+likeSchema.index({ artifact: 1, user: 1 }, { unique: true });
+
+export default mongoose.model("Like", likeSchema);
